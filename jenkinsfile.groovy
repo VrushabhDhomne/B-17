1463218d1b68428e82af29c32a59d72ce564d3fe
VrushabhDhomne <courseit32@gmail.com>
2022-05-21 20:13:17 +0530

    update grrovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,19 +5,35 @@ pipeline{
     stages{
         stage("git.pull"){
             steps{
-                echo "git pull from branch"
-                git branch: 'main', credentialsId: 'sshkey-worker', url: 'git@github.com:VrushabhDhomne/Devops.git'
+                sh 'sudo apt-get install update -y'
+                sh 'sudo apt-get install git -y '
+                git credentialsId: 'new-key', url: 'git@github.com:VrushabhDhomne/B-17.git'
                 sh 'ls'
             }
         }
-        stage("App.build"){
+        stage("Maven.build"){
             steps{
-                echo "App build from maven"
+                sh 'sudo apt-get install update -y'
+                sh 'sudo apt-get install maven -y'
+                sh 'mvn clean package'
             }
         }
-        stage("App.start"){
+        stage("Push.Artifact"){
             steps{
-                echo "git pull from branch"
+                sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
+                sh 'unzip awscliv2.zip'
+                sh 'sudo ./aws/install'
+                sh 'aws s3 cp test.war s3://dev-artifact/test2.war'
+            }
+        }
+        stage("Dev.Deployment"){
+            steps{
+                sh 'ssh -i jenkins.pem ec2-user@0.0.0.0'
+                sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
+                sh 'unzip awscliv2.zip'
+                sh 'sudo ./aws/install'
+                sh 'aws s3 cp s3://dev-artifact/test2.war /opt/tomcat/webapps'
+                sh './opt/tomcat/bin/startup.sh'
             }
         }
     }
