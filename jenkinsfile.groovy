7c272c119102e71c9075e682bae1a09940d3a534
VrushabhDhomne <courseit32@gmail.com>
2022-05-22 17:40:02 +0530

    update11

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -26,7 +26,7 @@ pipeline{
         stage("Dev.Deployment"){
             steps{
                 withCredentials([sshUserPrivateKey(credentialsId:'tomcat', keyFileVariable:'tomcat')]){
-                sh 'ssh -i ${tomcat} ec2-user@54.242.62.193'
+                sh 'ssh -i ${tomcat} -o StrictHostKeyChecking=no ec2-user@54.242.62.193'
                 sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
                 sh 'unzip awscliv2.zip'
                 sh 'sudo ./aws/install'
