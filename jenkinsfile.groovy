e6112d77b06332e56f272fd1548256b243611a41
VrushabhDhomne <courseit32@gmail.com>
2022-05-22 18:04:53 +0530

    update14

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -30,7 +30,7 @@ pipeline{
                 ssh -i ${tomcat} -o StrictHostKeyChecking=no ec2-user@54.242.62.193
                 aws s3 cp s3://dev-artifact-01/studentapp-${BUILD_ID}.war .
                 curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.78/bin/apache-tomcat-8.5.78.tar.gz
-                sudo tar -xvf apache-tomcat-8.5.78.tar.gz /opt/
+                sudo tar -xvf apache-tomcat-8.5.78.tar.gz -C /opt/
                 sudo sh /opt/apache-tomcat-8.5.78/bin/startup.sh
                 '''
                 }
