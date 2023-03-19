d6a2b13f340f6440e39161e7ebaf4e0e8feab087
Vrushab Dhomne <100924155+VrushabhDhomne@users.noreply.github.com>
2023-03-19 16:29:18 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline{
         } 
         stage("Push.Artifact"){
             steps{
-                sh 'sudo mv /var/lib/jenkins/workspace/ecs-pipeline/studentapp/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/studentapp-${BUILD_ID}.war'
+                sh 'sudo mv /var/lib/jenkins/workspace/ecs-pipeline/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/studentapp-${BUILD_ID}.war'
                 sh 'aws s3 cp /home/ubuntu/studentapp-${BUILD_ID}.war s3://dev-artifact-01'
             }
         }
