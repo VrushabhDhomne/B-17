2706f8136a79b5c369ab1e292a79fe77a0002eca
Vrushab Dhomne <100924155+VrushabhDhomne@users.noreply.github.com>
2023-03-19 17:14:45 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -19,16 +19,16 @@ pipeline{
         } 
         stage("Push.Artifact"){
             steps{
-                sh 'sudo mv /var/lib/jenkins/workspace/ecs-pipeline/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/studentapp-${BUILD_ID}.war'
-                sh 'aws s3 cp /home/ubuntu/studentapp-${BUILD_ID}.war s3://dev-artifact-01'
+                sh 'sudo mv /var/lib/jenkins/workspace/ecs-pipeline/target/studentapp-2.2-SNAPSHOT.war /home/ubuntu/studentapp.war'
+                sh 'aws s3 cp /home/ubuntu/studentapp.war s3://dev-artifact-01'
             }
         }
         stage("Dev.Deployment"){
             steps{
-                withCredentials([sshUserPrivateKey(credentialsId:'tomcat', keyFileVariable:'tomcat')]){
+                withCredentials([sshUserPrivateKey(credentialsId: 'tomcat', keyFileVariable: 'tomcat', usernameVariable: 'ubuntu')]) {
                 sh '''
-                ssh -i ${tomcat} -o StrictHostKeyChecking=no ec2-user@54.242.62.193<<EOF
-                sudo aws s3 cp s3://dev-artifact-01/studentapp-${BUILD_ID}.war .
+                ssh -i ${tomcat} -o StrictHostKeyChecking=no ubuntu@100.25.149.61<<EOF
+                sudo aws s3 cp s3://dev-artifact-01/studentapp.war .
                 curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.78/bin/apache-tomcat-8.5.78.tar.gz
                 sudo tar -xvf apache-tomcat-8.5.78.tar.gz -C  /opt/
                 sudo sh /opt/apache-tomcat-8.5.78/bin/shutdown.sh
