f3c92ae46bb28135cecec9190987ce6a16656c17
Vrushab Dhomne <100924155+VrushabhDhomne@users.noreply.github.com>
2023-03-19 18:35:57 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -28,7 +28,7 @@ pipeline{
                 withCredentials([sshUserPrivateKey(credentialsId: 'sam', keyFileVariable: 'sam', usernameVariable: 'apple')]) {
                 sh '''
                 ssh -i ${sam} -o StrictHostKeyChecking=no ubuntu@34.239.248.122<<EOF
-                sudo aws s3 cp s3://dev-artifact-01/studentapp13.war .
+                sudo aws s3 cp s3://dev-artifact-01/studentapp-13.war .
                 wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.8/bin/apache-tomcat-10.0.8.tar.gz
                 sudo tar xzvf apache-tomcat-10.0.8.tar.gz  /opt/
                 sudo sh /opt/apache-tomcat-8.5.78/bin/shutdown.sh
