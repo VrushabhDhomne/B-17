4097090cc91a96f262080c9748e5f6270a93932e
Vrushab Dhomne <100924155+VrushabhDhomne@users.noreply.github.com>
2023-03-19 19:09:03 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -31,7 +31,7 @@ pipeline{
                 sudo aws s3 cp s3://dev-artifact-01/studentapp-13.war .
                 wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.87/bin/apache-tomcat-8.5.87.tar.gz
                 sudo tar -xvf apache-tomcat-8.5.87.tar.gz -C /opt/
-                sudo sh /opt/apache-tomcat-8.5.78/bin/shutdown.sh
+                sudo sh /opt/apache-tomcat-8.5.87/bin/shutdown.sh
                 sudo cp -rv studentapp13.war studentapp.war
                 sudo cp -rv studentapp.war /opt/apache-tomcat-8.5.78/webapps/
                 sudo sh /opt/apache-tomcat-8.5.78/bin/startup.sh
