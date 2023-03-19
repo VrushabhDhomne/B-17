86753a4452f04d48b0491b643f827a80395e2ee3
Vrushab Dhomne <100924155+VrushabhDhomne@users.noreply.github.com>
2023-03-19 18:59:31 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -29,8 +29,8 @@ pipeline{
                 sh '''
                 ssh -i ${sam} -o StrictHostKeyChecking=no ubuntu@34.239.248.122<<EOF
                 sudo aws s3 cp s3://dev-artifact-01/studentapp-13.war .
-                wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.8/bin/apache-tomcat-10.0.8.tar.gz
-                sudo tar xzvf apache-tomcat-10.0.8.tar.gz  /opt/
+                wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.87/bin/apache-tomcat-8.5.87.tar.gz
+                sudo tar -xvf apache-tomcat-8.5.78.tar.gz -C /opt/
                 sudo sh /opt/apache-tomcat-8.5.78/bin/shutdown.sh
                 sudo cp -rv studentapp13.war studentapp.war
                 sudo cp -rv studentapp.war /opt/apache-tomcat-8.5.78/webapps/
